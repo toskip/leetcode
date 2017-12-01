693f08631231d2228e0e05086cfe32dbdedaa303
nemosc <rain_sys@126.com>
2017-12-02 00:17:26 +0800

    Update 49_Group Anagrams.cpp

--- 49_Group Anagrams.cpp
+++ 49_Group Anagrams.cpp
@@ -1,34 +1,26 @@
-//击败了3%..rnmb
+//最普通的
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> result;
-        vector<string> hash;
-        int count = 1;
+        map<string,int> hash;
+        int count = 0;
         for(int i=0;i<strs.size();i++)
         {
             int sum = 0;
             string temp = strs[i];
             sort(temp.begin(),temp.end());
-            int index = -1;
-            for(int j=0;j<hash.size();j++)
+            if(hash.find(temp)==hash.end())
             {
-                if(hash[j]==temp)
-                {
-                    index = j;
-                    break;
-                }
-            }
-            if(index ==-1)
-            {
-                hash.push_back(temp);
+                //meizhaodao
+                hash[temp]=count++;
                 vector<string> current;
                 current.push_back(strs[i]);
                 result.push_back(current);
             }
             else
             {
-                result[index].push_back(strs[i]);
+                result[hash[temp]].push_back(strs[i]);
             }
         }
         return result;
