b4f0dfa5c609ebcf866a5a69d549e962c3b8a454
nemosc <rain_sys@126.com>
2017-12-02 16:43:37 +0800

    Create 55_Jump Game_greedy.cpp

---
+++ 55_Jump Game_greedy.cpp
@@ -0,0 +1,19 @@
+//Greedy method in Solution#4
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        bool ok[30000] = {0};
+        int firstgoodindex = nums.size()-1;
+        ok[firstgoodindex] = 1; 
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            if(i+nums[i]>=firstgoodindex)
+            {
+                firstgoodindex = i;
+                ok[i] =1;
+            }
+        }
+        if(ok[0]) return true;
+        else return false;
+    }
+};
