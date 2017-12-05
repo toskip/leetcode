91b7e80adb99f3e849e7ea1ac7f6a5e726cd8d4c
nemosc <rain_sys@126.com>
2017-12-05 11:07:35 +0800

    Update 63_Unique Paths II_dp.cpp

--- 63_Unique Paths II_dp.cpp
+++ 63_Unique Paths II_dp.cpp
@@ -1,20 +1,17 @@
-//easy
+//参照答案重写了一下结构，果然这样比较好
 int dp[101][101];
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        for(int i=1;i<=m;i++)
+        dp[0][1] = 1;
+        for(int i=1;i<=m;++i)
         {
-            for(int j = 1;j<=n;j++)
+            for(int j = 1;j<=n;++j)
             {
                 if(obstacleGrid[i-1][j-1]==1) dp[i][j] = 0;
-                else 
-                {
-                    if(i==1 && j==1) dp[i][j] =1;
-                    else dp[i][j] = dp[i-1][j]+dp[i][j-1];
-                }
+                else dp[i][j] = dp[i-1][j]+dp[i][j-1];
             }
         }
         return dp[m][n];
