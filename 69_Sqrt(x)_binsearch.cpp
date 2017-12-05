cda381f965199324699488018b02eee585863435
nemosc <rain_sys@126.com>
2017-12-05 16:31:17 +0800

    Create 69_Sqrt(x)_binsearch.cpp

---
+++ 69_Sqrt(x)_binsearch.cpp
@@ -0,0 +1,22 @@
+//二分查找
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0) return 0;
+        int low = 1;
+        int high = x;
+        int mid = (high+low)/2;
+        while(true)
+        {
+            //cout<<low<<' '<<high<<' '<<mid<<endl;
+            mid =(high+low)/2;
+            if(mid>x/mid) high =mid-1;
+            else
+            {            
+                if (mid + 1 > x/(mid + 1)) return mid;
+                low = mid+1;
+            }
+            
+        }
+    }
+};
