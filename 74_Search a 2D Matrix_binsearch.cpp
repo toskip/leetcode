e150d8ab276e8c0fa3f3c2bc07a6faa045524fe1
nemosc <rain_sys@126.com>
2017-12-06 13:06:29 +0800

    Update 74_Search a 2D Matrix_binsearch.cpp

--- 74_Search a 2D Matrix_binsearch.cpp
+++ 74_Search a 2D Matrix_binsearch.cpp
@@ -1,35 +1,29 @@
-//想一想二分查找怎么写最简洁又不容易出错
+//记住这个写法吧 我觉得这是二分查找最好的写法
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         if(m==0) return false;
         int n  = matrix[0].size();
-        if(n==0) return false;
         int  num = m*n;
         int low = 0;
         int high = num-1;
-        int mid = (low+high)/2;
-        int i = mid/n;
-        int j = mid%n;
-        while(low<high)
+        int mid,i,j;
+        while(low<=high)
         {
-            //cout<<low<<' '<<high<<' '<<mid<<' '<<i<<' '<<j<<endl;
+            mid= (low+high)/2;
+            i = mid/n;
+            j = mid%n;
             if(matrix[i][j]==target) return true;
             else if(matrix[i][j]>target)
             {
-                high = mid;
+                high = mid-1;
             }
             else
             {
                 low = mid+1;
             }
-            mid= (low+high)/2;
-            i = mid/n;
-            j = mid%n;
-            //cout<<low<<' '<<high<<' '<<mid<<' '<<i<<' '<<j<<endl;
         }
-        if(matrix[i][j]==target) return true;
         return false;
     }
 };
