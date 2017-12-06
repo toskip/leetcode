62ad943e9e0ccfc83c96598110307eddb79e05cb
nemosc <rain_sys@126.com>
2017-12-06 13:10:50 +0800

    Create binsearch.cpp

---
+++ binsearch.cpp
@@ -0,0 +1,24 @@
+//记住这个写法吧 我觉得这是二分查找最好的写法
+class Solution {
+public:
+    bool binSearch(vector<int>& a, int target) {
+        int  num = a.size();
+        int low = 0;
+        int high = num-1;
+        int mid;
+        while(low<=high)
+        {
+            mid= (low+high)/2;
+            if(a[mid]==target) return true;
+            else if(a[mid]>target)
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+        return false;
+    }
+};
