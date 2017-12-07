476eb16e20c62d4003261f2a4ff2bfc1ca112400
nemosc <rain_sys@126.com>
2017-12-07 23:27:45 +0800

    Create  14_Longest Common Prefix.cpp

---
+++ 14_Longest Common Prefix.cpp
@@ -0,0 +1,17 @@
+//O(mn)
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==0) return "";
+        string prefix = "";
+        for(int i = 0;i<strs[0].size();i++)
+        {
+            for(int j = 1;j<strs.size();j++)
+            {
+                if(strs[j].size()<=i||strs[j][i]!=strs[0][i]) return prefix;
+            }
+            prefix+=strs[0][i];
+        }
+        return prefix;
+    }
+};
