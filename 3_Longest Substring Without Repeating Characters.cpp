859cc4abd2bac8347bec41497f07a67afe889ed4
nemosc <rain_sys@126.com>
2017-12-08 15:26:51 +0800

    Update 3_Longest Substring Without Repeating Characters.cpp

--- 3_Longest Substring Without Repeating Characters.cpp
+++ 3_Longest Substring Without Repeating Characters.cpp
@@ -1,27 +1,21 @@
-//dp方法，O(n)
+//好吧原来不需要dp，太蠢了
+//O(n)
 int indices[128];
 class Solution
 {
 public:
     int lengthOfLongestSubstring(string s)
     {
-        int n = s.size();
-        int* dp = new int[n];
-        memset(dp,0,sizeof(dp));
         memset(indices,-1,sizeof(indices));
-        dp[0] = 1;
-        indices[s[0]] = 0;
+        int n = s.size();
+        int maxlen = 0;
         int startindex = 0;
-        for(int i = 1;i<n;i++)
+        for(int i = 0;i<n;i++)
         {
-            if(indices[s[i]]>=startindex)
-            {
-                startindex = indices[s[i]]+1;
-                dp[i] = dp[i-1];
-            }
-            else dp[i] = max(dp[i-1],i-startindex+1);
+            if(indices[s[i]]>=startindex) startindex = indices[s[i]]+1;
+            else maxlen = max(maxlen,i-startindex+1);
             indices[s[i]] =i;
         }
-        return dp[n-1];
+        return maxlen;
     }
 };
