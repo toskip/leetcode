7d698d7542e7957ba6cc6fc3fed1a74d36a96018
nemosc <rain_sys@126.com>
2017-12-09 21:18:08 +0800

    Update 30_Substring with Concatenation of All Words_short.cpp

--- 30_Substring with Concatenation of All Words_short.cpp
+++ 30_Substring with Concatenation of All Words_short.cpp
@@ -1,4 +1,4 @@
-//too slow
+//击败了40%，加油，继续优化
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
@@ -8,8 +8,9 @@ public:
         int n = s.size();
         int p = words.size();
         int q = words[0].size();
+        int max = n-p*q;
         for(int i = 0;i<p;i++) ++haxi[words[i]];
-        for(int i = 0;i<n;i++)
+        for(int i = 0;i<=max;i++)
         {
             haxi2 = haxi;
             bool ok = true;
