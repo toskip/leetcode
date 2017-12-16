2b2a04f01998274e5241838d501ee094637b8cbb
nemosc <rain_sys@126.com>
2017-12-17 00:17:48 +0800

    Update 78_Subsets.cpp

--- 78_Subsets.cpp
+++ 78_Subsets.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
+    int n;
     vector<vector<int>> result;
     vector<int> current;
-    int n,k;
-    void dfs(int start,int numbers,vector<int>& nums)
+    void dfs(int start,int remain,vector<int>& nums)
     {
-        if(numbers==k)
-        {
-            result.push_back(current);
-            return;
-        }
+        if(remain==0) result.push_back(current);
         for(int i = start;i<n;i++)
         {
-                current.push_back(nums[i]);
-                dfs(i+1,numbers+1,nums);
-                current.pop_back();
+            current.push_back(nums[i]);
+            dfs(i+1,remain-1,nums);
+            current.pop_back();
         }
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         n = nums.size();
-        for(int i = 0;i<=n;i++)
-        {
-            k = i;
-            dfs(0,0,nums);
-        }
+        for(int i = 0;i<=n;i++) dfs(0,i,nums);
         return result;
     }
 };
