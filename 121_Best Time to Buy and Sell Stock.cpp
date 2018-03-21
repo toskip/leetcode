0e63fb1a8f13609d8b2a46a667aa843be900c539
nemosc <rain_sys@126.com>
2018-03-21 18:16:25 +0800

    Update 121_Best Time to Buy and Sell Stock.cpp

--- 121_Best Time to Buy and Sell Stock.cpp
+++ 121_Best Time to Buy and Sell Stock.cpp
@@ -1,22 +1,13 @@
-
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        if(!n) return 0;
-        int amin[30000];
-        int amax[30000];
-        amin[0] = prices[0];
-        amax[n-1] = prices[n-1];
-        for(int i = 1;i<n;i++)
-        {
-            amin[i] = min(amin[i-1],prices[i]);
-            amax[n-1-i] = max(amax[n-i],prices[n-1-i]);
-        }
+        int minvalue = INT_MAX;
         int result = 0;
-        for(int i =0;i<n;i++)
+        for(int i = 0;i<n;i++)
         {
-            result = max(result,amax[i]-amin[i]);
+            minvalue = min(minvalue,prices[i]);
+            result = max(result,prices[i]-minvalue);
         }
         return result;
     }
