6b96fcafc05c4b4105e795631e4c887e08fce5a2
nemosc <rain_sys@126.com>
2018-03-21 14:47:13 +0800

    Update 115_Distinct Subsequences.cpp

--- 115_Distinct Subsequences.cpp
+++ 115_Distinct Subsequences.cpp
@@ -8,13 +8,13 @@ public:
         int lent = t.size();
         int lens = s.size();
         for(int i = 0;i<=lent;i++) dp[i][0] = 0;
-        for(int j =0;j<=lens;j++) dp[0][j] = 1;
+        for(int j = 0;j<=lens;j++) dp[0][j] = 1;
         for(int i = 1;i<=lent;i++)
         {
             for(int j = 1;j<=lens;j++)
             {
-                if(t[i-1]==s[j-1]) dp[i][j] = dp[i][j-1]+dp[i-1][j-1];
-                else dp[i][j] = dp[i][j-1];
+                dp[i][j] = dp[i][j-1];
+                if(t[i-1]==s[j-1]) dp[i][j] += dp[i-1][j-1];
             }
         }
         return dp[lent][lens];
