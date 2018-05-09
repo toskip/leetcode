613b048421bac06524611f51a1beabea9520abf6
neomsc <rain_sys@126.com>
2018-05-09 15:26:21 +0800

    Update 135_Candy.py

--- 135_Candy.py
+++ 135_Candy.py
@@ -1,4 +1,5 @@
-#TLE了，下午接着改，优化一下不难
+#想了半天 应该是最优解法了
+#代码和思路都很elegent，超有成就感有木有
 class Solution:
     def candy(self, ratings):
         """
@@ -6,19 +7,12 @@ class Solution:
         :rtype: int
         """
         n = len(ratings)
-        give = [0]*n
-        give[0] =1
+        increase = [1]*n
+        decrease = [1]*n
         for i in range(1,n):
-            if ratings[i]>ratings[i-1]:
-                give[i] = give[i-1]+1
-            else:
-                give[i] =1
-                j = i
-                while j>0 and ratings[j-1]>ratings[j] and give[j-1]==give[j]:
-                    give[j-1]+=1
-                    j-=1
+            if ratings[i]>ratings[i-1]: increase[i] = increase[i-1]+1
+            if ratings[n-i-1]>ratings[n-i]: decrease[n-1-i] = decrease[n-i]+1
         result = 0
-        #print(give)
         for i in range(n):
-            result+=give[i]
+            result += max(increase[i],decrease[i])
         return result
