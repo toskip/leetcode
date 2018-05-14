b6e288c71ebf1005af7b71d22af00eaf2c8925a8
neomsc <rain_sys@126.com>
2018-05-14 17:42:13 +0800

    Update 207_Course Schedule.py

--- 207_Course Schedule.py
+++ 207_Course Schedule.py
@@ -8,21 +8,20 @@ class Solution:
         """
         count = [0]*numCourses
         q = queue.Queue()
+        al = [[] for i in range(numCourses)]
         total = 0
         for each in prerequisites:
-            count[each[1]]+=1
+            count[each[0]]+=1
+            al[each[1]].append(each[0])
         for i in range(numCourses):
             if count[i]==0:
                 q.put(i)
                 total+=1
         while not q.empty():
             cur = q.get()
-            for each in prerequisites:
-                if each[0]==cur:
-                    count[each[1]]-=1
-                    if count[each[1]]==0:
-                        q.put(each[1])
-                        total+=1
-        if total == numCourses: return True
-        else: return False
-            
+            for each in al[cur]:
+                count[each]-=1
+                if count[each]==0:
+                    q.put(each)
+                    total+=1
+        return total == numCourses
