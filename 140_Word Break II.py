02728647b0945059aa726728a8e09e12f9dd96c2
neomsc <rain_sys@126.com>
2018-06-09 17:14:57 +0800

    Update 140_Word Break II.py

--- 140_Word Break II.py
+++ 140_Word Break II.py
@@ -1,5 +1,21 @@
-//递归，超时
 class Solution:
+    def wordBreak0(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        wordDict = set(wordDict)
+        n = len(s)
+        dp = [False]*(n+1)
+        dp[0] = True
+        for i in range(1,n+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+        #print(dp)
+        return dp[n]
     def wordBreak(self, s, wordDict):
         """
         :type s: str
@@ -9,7 +25,8 @@ class Solution:
         self.wordDict = wordDict
         self.result = []
         self.cur = []
-        self.recur(s)
+        if self.wordBreak0(s,wordDict):
+            self.recur(s)
         return self.result
     
     def recur(self,s):
