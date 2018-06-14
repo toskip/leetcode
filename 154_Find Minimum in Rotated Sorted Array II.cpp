2b98ab911f583bb4b19d050883474d3f1492d6f4
neomsc <rain_sys@126.com>
2018-06-14 14:57:35 +0800

    Create 154_Find Minimum in Rotated Sorted Array II.cpp

---
+++ 154_Find Minimum in Rotated Sorted Array II.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size()-1;
+        int mid;
+        int result = INT_MAX;
+        while(low<=high)
+        {
+            mid = (low+high)/2;
+            result = min(result,nums[mid]);
+            if(nums[low]==nums[high])
+            {
+                low++;
+                continue;
+            }
+            if(nums[mid]>nums[high])
+            {
+                low = mid+1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+            
+        }
+        return result;
+        
+    }
+};
