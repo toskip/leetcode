c7b372985916ae3c1ae5888c85f264a246dd8957
neomsc <rain_sys@126.com>
2018-08-07 12:43:16 +0800

    Update 3_Longest Substring Without Repeating Characters.cpp

--- 3_Longest Substring Without Repeating Characters.cpp
+++ 3_Longest Substring Without Repeating Characters.cpp
@@ -1,22 +1,22 @@
-//好吧原来不需要dp，太蠢了
-//记录下每个字符最后一次出现的位置与当前子序列的开始
-//O(n)
-int indices[128];
-class Solution
-{
+class Solution {
 public:
-    int lengthOfLongestSubstring(string s)
-    {
-        memset(indices,-1,sizeof(indices));
-        int n = s.size();
-        int maxlen = 0;
-        int startindex = 0;
+    int lengthOfLongestSubstring(string s) {
+        int n  = s.size();
+        int size = 0;//滑动窗口大小
+        unordered_map<char,bool> dict;
+        int result = 0;
         for(int i = 0;i<n;i++)
         {
-            if(indices[s[i]]>=startindex) startindex = indices[s[i]]+1;
-            else maxlen = max(maxlen,i-startindex+1);
-            indices[s[i]] =i;
+            while(dict[s[i]])
+            {
+                dict[s[i-size]] = 0;
+                size--;
+            }
+            dict[s[i]] = 1;
+            size++;
+            result = max(result,size);
         }
-        return maxlen;
+        return result;
+        
     }
 };
