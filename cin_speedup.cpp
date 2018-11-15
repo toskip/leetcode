578af35d6c42bdf1a60cb0752764d63573b74f13
0sys0 <rain_sys@126.com>
2018-11-15 13:39:58 +0800

    Update cin_speedup.cpp

--- cin_speedup.cpp
+++ cin_speedup.cpp
@@ -3,3 +3,10 @@ static vector<int> height=[](){
     cin.tie(NULL);
     return vector<int>{};
 }();
+
+static int speedUp = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
