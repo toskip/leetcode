ad00a88a32d426d9fc2f31035023b37561f174dc
0sys0 <rain_sys@126.com>
2018-11-15 13:38:33 +0800

    Update 201_Bitwise AND of Numbers Range.cpp

--- 201_Bitwise AND of Numbers Range.cpp
+++ 201_Bitwise AND of Numbers Range.cpp
@@ -2,27 +2,52 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int m, int n) {
-        if(m==0) return 0;
-        bitset<33> temp(m);
+        bitset<32> temp(m);
         int i = 0;
-        while(i<33)
+        unsigned int current;
+        for(int i = 0;i<32;)
         {
             if(temp[i]==0)
             {
                 i++;
                 continue;
             }
-            while(temp[i]==1 && i<33) 
+            while(temp[i]==1) 
             {
                 temp[i] = 0;
                 i++;
                 
             }
             temp[i] = 1;
-            unsigned int current = temp.to_ulong();
+            current = temp.to_ulong();
             if(current>n) break;
             m&=current;
         }
         return m;
     }
 };
+
+/*
+//标准答案的方法
+class Solution {
+public:
+    long long rangeBitwiseAnd(int m, int n) {        
+        int cnt = 0;
+
+        while(n != 0 && m != 0) {
+            if (n == m) {
+                long long r = n;
+
+                return (r << cnt);
+            }
+            
+            n /= 2;
+            m /= 2;
+            cnt+=1;
+        }
+        
+        
+        return 0;
+    }
+};
+*/
