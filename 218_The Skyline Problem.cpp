9fab6acca45f4d71ac97c85edf1bc056cbccc83b
0sys0 <rain_sys@126.com>
2019-01-10 21:16:43 +0800

    Update 218_The Skyline Problem.cpp

--- 218_The Skyline Problem.cpp
+++ 218_The Skyline Problem.cpp
@@ -2,7 +2,6 @@
 class Solution {
 public:
 	vector<pair<int, int>> result;
-
 	vector<pair<int, int>> getSkyline(vector<vector<int>>& buildings) {
 		int startpos = 0;
 		for (int i = 0; i < buildings.size(); i++)
@@ -12,15 +11,12 @@ public:
 				startpos++;
 			}
 			//比较前面一个关键点和第i个建筑物起点哪个高
-			if (startpos>0 && buildings[i][2] < result[startpos - 1].second)
+			if (startpos == 0 || buildings[i][2] >= result[startpos - 1].second)
 			{
-				//对于起点，什么都不做
-			}
-			else
-			{
-				//把起点拔高然后过
+				
 				if (startpos > 0 &&  buildings[i][0] == result[startpos - 1].first)
 				{
+					//把起点拔高
 					result[startpos - 1].second = buildings[i][2];
 				}
 				else
@@ -28,7 +24,6 @@ public:
 					//把起点作为关键点插入
 					result.insert(result.begin() + startpos, make_pair(buildings[i][0], buildings[i][2]));
 				}
-				
 			}
 		//把第i个建筑物起点和终点之间低的点都拔高
 			int endpos = startpos;
@@ -40,30 +35,21 @@ public:
 					temp = result[endpos].second;
 					result[endpos].second = buildings[i][2];
 				}
-				endpos++;			}
+				endpos++;			
+			}
 			if (endpos == result.size()) //第i个建筑物的终点在所有的关键点后面
 			{
 				//直接插到最后，加入关键点
 				result.push_back(make_pair(buildings[i][1], 0));
 			}
-			else if (buildings[i][1] == result[endpos].first)
-			{
-
-			}
-			else
+			else if(buildings[i][1] != result[endpos].first)
 			{
 				//比较前面一个关键点和第i个建筑物终点哪个高
-				if (buildings[i][2] < result[endpos - 1].second)
-				{
-					//对于终点，什么都不做
-				}
-				else
+				if (buildings[i][2] >= result[endpos - 1].second)
 				{
 					//把终点作为关键点插入
 					if (temp == -1) temp = result[startpos - 1].second;
 					result.insert(result.begin() + endpos, make_pair(buildings[i][1], temp));
-
-
 				}
 			}
 		}
@@ -76,7 +62,6 @@ public:
 			result.erase(result.begin()+i,result.begin()+j);
 			i++;
 		}
-
 		return result;
 	}
 };
