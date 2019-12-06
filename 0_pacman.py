9873ac84ba76058b5124756eb5442afe86d63304
0sys0 <rain_sys@126.com>
2019-12-06 22:04:51 +0800

    Create 0_pacman.py

---
+++ 0_pacman.py
@@ -0,0 +1,2 @@
+r = 20
+for i in range(r*2):print(' '.join(['*' if (i-r)*(i-r)+(j-r)*(j-r)<r*r and (j<=r or abs((i-r)/(j-r))>0.5) and (j-r)*(j-r)+(i-r/2)*(i-r/2)>=(r/8)*(r/8) else ' ' for j in range(r*2)]))
