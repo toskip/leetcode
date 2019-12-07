74031d236ef4afa130379fbba2ce600072aef135
0sys0 <rain_sys@126.com>
2019-12-07 23:22:01 +0800

    Update 0_pacman.py

--- 0_pacman.py
+++ 0_pacman.py
@@ -1,8 +1,8 @@
 import sys
 r = 30
 k = 0.5
-inc = 0.04
+step = 0.04
 while True:
     sys.stdout.write('\n'.join([' '.join(['*' if (i-r)*(i-r)+(j-r)*(j-r)<r*r and (j<=r or abs((i-r)/(j-r))>k) and (j-r)*(j-r)+(i-r/2)*(i-r/2)>=(r/8)*(r/8) else ' ' for j in range(r*2)])for i in range(r*2)]))
-    k+=inc
-    inc = -inc if k>=1.5 or k<=0.2 else inc
+    k+=step
+    step = -step if k>=1.5 or k<=0.2 else step
