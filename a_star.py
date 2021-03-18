3db6621e932fa0a03697cc5409f13a660428557a
nemosc <rain_sys@126.com>
2021-03-18 11:34:44 +0800

    Update a_star.py

--- a_star.py
+++ a_star.py
@@ -1,44 +1,56 @@
-#未完成
 from heapq import * 
+import time
 class AStar:
     def __init__(self):
-        self._map = [[0,0,0,0,0,0,0],[0,0,0,9,0,0,0],[0,1,0,9,0,2,0],[0,0,0,9,0,0,0],[0,0,0,0,0,0,0]]
-        self._d = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
-        self._f = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
-        self._g = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
-        self._h = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
+        #显示地图
+        self._map = [[0,0,0,0,0,0,0],[0,0,0,9,0,0,0],[0,0,0,9,0,0,0],[0,1,0,9,0,2,0],[0,0,0,9,0,0,0],[0,0,0,0,0,0,0]]
+        #方向
+        self._d = [[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]],[[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]]
+        #f g h
+        self._f = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
+        self._g = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
+        self._h = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
         self._d1 = [[1,0],[0,1],[-1,0],[0,-1]] #右下左上
         self._d2 = [[1,0],[0,1],[-1,0],[0,-1],[1,1],[-1,1],[-1,-1],[1,-1]] #右下左下左上右上
-        self._start = [2,1]
-        self._target= [2,5]
+        self._arrow = {str([1,0]):'→',str([0,1]):'↓',str([-1,0]):'←',str([0,-1]):'↑',str([0,0]):'○'}
+        self._start = [3,1]
+        self._target= [3,5]
         self._open_list = []
         self._close_list = []
         self._open_list.append(self._start)
     def run(self):
-        while len(self._open_list)!=0:
+        while len(self._open_list)!=0 and self._target not in self._open_list:
             # find the smallest one
-            current = heappop(self._open_list)
+            current = min(self._open_list,key=lambda x:self._f[x[0]][x[1]])
+            current_i = self._open_list.index(current)
+            self._open_list = self._open_list[:current_i]+self._open_list[current_i+1:]
+
             self._close_list.append(current)
             for d in self._d1:
                 nex = [current[0]+d[0],current[1]+d[1]]
-                if nex[0]>=0 and nex[0]<5 and nex[1]>=0 and nex[1]<7 and self._map[nex[0]][nex[1]]!=9 and nex not in self._close_list:
+                if nex[0]>=0 and nex[0]<6 and nex[1]>=0 and nex[1]<7 and self._map[nex[0]][nex[1]]!=9 and nex not in self._close_list:
                     if nex not in self._open_list:
-                        heappush(self._open_list,nex)
+                        self._open_list.append(nex)
                         self._g[nex[0]][nex[1]] = self._g[current[0]][current[1]]+1
                         self._h[nex[0]][nex[1]] = self._distance(nex,self._target)
-                        self._d[nex[0]][nex[1]] = str(d)
+                        self._d[nex[0]][nex[1]] = str([-d[0],-d[1]])
                     else:
                         if self._g[current[0]][current[1]]+1<self._g[nex[0]][nex[1]]:
                             self._g[nex[0]][nex[1]] = self._g[current[0]][current[1]]+1
-                            self._d[nex[0]][nex[1]] = str(d)
+                            self._d[nex[0]][nex[1]] = str([-d[0],-d[1]])
                     self._f[nex[0]][nex[1]] = self._g[nex[0]][nex[1]]+self._h[nex[0]][nex[1]]
             print('\n'.join(' '.join(['%2d' % i for i in row]) for row in self._map))
             print('')
             print('\n'.join(' '.join(['%2d' % i for i in row]) for row in self._f))
             print('')
-            print('\n'.join(' '.join([str(i) for i in row]) for row in self._d))
+            print('\n'.join(' '.join(['%2d' % i for i in row]) for row in self._g))
+            print('')
+            print('\n'.join(' '.join(['%2d' % i for i in row]) for row in self._h))
+            print('')
+            print('\n'.join(' '.join([self._arrow[str(i)] for i in row]) for row in self._d))
             print(self._open_list)
             print(self._close_list)
+            #time.sleep(1)
         
             
 
